7287618f07a67d9225c51f0efeb5fdafbbdc07a4
Gerson-Antunes <antuness.gerson@gmail.com>
2025-10-04 20:02:22 +0930

    Add files via upload

---
+++ AppProject.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CustomerVehicleCalculatorApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppProject : Page
+    {
+        public AppProject()
+        {
+            this.InitializeComponent();
+        }
+
+        // Save button event handling code
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            phoneTextBox.IsEnabled = false;
+            customerNameTextBox.IsEnabled = false;
+            vehiclePriceTextBox.Focus(FocusState.Programmatic);
+        }
+
+        // Reset button event handling code
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            phoneTextBox.Text = "";
+            customerNameTextBox.Text = "";
+            vehiclePriceTextBox.Text = "";
+            tradeInTextBox.Text = "";
+            subAmountTextBox.Text = "";
+            gstAmountTextBox.Text = "";
+            finalAmountTextBox.Text = "";
+            phoneTextBox.IsEnabled = true;
+            customerNameTextBox.IsEnabled = true;
+            customerNameTextBox.Focus(FocusState.Programmatic);
+        }
+
+        // Summary Button event handling code
+
+        private void summaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            const decimal GST_RATE = 0.1m;
+            decimal subAmount;
+            decimal gstAmount;
+            decimal finalAmount;
+
+            subAmount = decimal.Parse(vehiclePriceTextBox.Text) - decimal.Parse(tradeInTextBox.Text);
+            subAmountTextBox.Text = subAmount.ToString("C");
+
+            gstAmount = subAmount * GST_RATE;
+            gstAmountTextBox.Text = gstAmount.ToString("C");
+
+            finalAmount = subAmount + gstAmount;
+            finalAmountTextBox.Text = finalAmount.ToString("C");
+
+            summaryTextBlock.Text = "Summary data displayed here";
+        }
+    }
+}
